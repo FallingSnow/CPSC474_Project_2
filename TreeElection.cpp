d4766d6d07ddeb427104ccacce64099ab26a754e
bsa919adam <bsa919adam@gmail.com>
2018-11-27 13:04:03 -0800

    Update TreeElection.cpp

--- TreeElection.cpp
+++ TreeElection.cpp
@@ -1,27 +1,54 @@
 #include <stdio.h>
 #include "mpi.h"
-
+#include <vector>
+struct node{
+    int rank=-1;
+    bool parent =False;
+    bool msgRecived= False;
+    bool child =False;
+   
+}
+bool checkRcvs(vector<node*> x){
+    int count=0;
+    for(int i=0; i< x.size(); i++){
+        if(!(x[i]->msgRecived)){
+            count++;   
+        }
+    }
+    
+}
 int main( int argc, char *argv[] )
 {
     int rank, size;
     MPI_Init( &argc, &argv );
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
     MPI_Comm_size( MPI_COMM_WORLD, &size );
-    int parent;
-    int rightc;
-    int leftc;
+    vector<node*> neighbors; 
+    bool rightReceive= False;
+    bool leftReceive= False;
     if(rank!=0){
-      parent=(rank-1)/2;
-      rightc=2*rank+1;
-      leftc=2*rank+2;
+        neighbors.push_back(new node);
+        neighbors.back()->rank= (rank-1)/2;
     }
-   if(rightc>=size){
-    rightc=-1;    
-   }
-   if(leftc>=size){
-    leftc=-1;
-   }
-   
+    
+    int rightc=2*rank+1;
+    int leftc=2*rank+2;
+    
+    if(rightc<size){
+        neighbors.push_back(new node);
+        neighbors.back()->rank=rightc;     
+    }
+    if(leftc<size){
+        neighbors.push_back(new node);
+        neighbors.back()->rank=leftc;    
+    }
+    while(!rightReceive && !leftReceive){
+        //receive msg   
+    }
+    if(leftc==-1 && rightc=-1){
+        //send msg to parent       
+    }
+    
     MPI_Finalize();
     return 0;
 }
